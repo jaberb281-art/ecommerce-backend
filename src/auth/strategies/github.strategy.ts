2b18836a1a54d6652e828a237342dd7cf2e01212
Jaber Ali <jaberb281@gmail.com>
2026-04-01 19:47:17 +0500

    fix: githubStrategy

--- src/auth/strategies/github.strategy.ts
+++ src/auth/strategies/github.strategy.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-github2';
-import { ConfigService } from '@nestjs/config'; // Recommended to use ConfigService
-
 @Injectable()
 export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
     // src/auth/strategies/github.strategy.ts
     constructor() {
         super({
-            clientID: process.env.GITHUB_CLIENT_ID!,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-            callbackURL: process.env.GITHUB_CALLBACK_URL!,
+            clientID: 'Ov23liwh0pUPL1zRXa7y',
+            clientSecret: '25d22fec2e3cd8d3fa080d72c7ce8a13b64c01ed',
+            callbackURL: 'https://ecommerce-backend-cqoc.vercel.app/api/auth/callback/github',
             scope: ['user:email'],
         });
 
