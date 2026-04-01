23f8c673961cbca0310a3fa95737b41b61b1c734
Jaber Ali <jaberb281@gmail.com>
2026-04-01 18:25:41 +0500

    feat: fix github oauth strategy and add missing routes/service logic

--- src/auth/strategies/github.strategy.ts
+++ src/auth/strategies/github.strategy.ts
@@ -5,14 +5,16 @@ import { ConfigService } from '@nestjs/config'; // Recommended to use ConfigServ
 
 @Injectable()
 export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
-    constructor(private configService: ConfigService) {
+    // src/auth/strategies/github.strategy.ts
+    constructor() {
         super({
-            // Directly paste the strings here without configService.get()
             clientID: 'Ov23liwh0pUPL1zRXa7y',
             clientSecret: '25d22fec2e3cd8d3fa080d72c7ce8a13b64c01ed',
+            // NestJS adds /api automatically if it's in main.ts, but Passport needs the full absolute URL
             callbackURL: 'https://ecommerce-backend-cqoc.vercel.app/api/auth/callback/github',
             scope: ['user:email'],
         });
+
     }
 
     async validate(accessToken: string, refreshToken: string, profile: any) {
