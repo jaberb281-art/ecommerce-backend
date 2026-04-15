0bbada458b82e303b214fc64bcd098247b98c108
Jaber Ali <jaberb281@gmail.com>
2026-04-15 22:34:57 +0500

    fix: few bugs

--- prisma/prisma.config.js
+++
@@ -1,9 +0,0 @@
-// prisma/prisma.config.ts
-import 'dotenv/config';
-
-export default {
-    engine: 'classic',
-    datasource: {
-        url: process.env.DATABASE_URL,
-    },
-};
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      // Prisma 7 uses 'datasourceUrl' instead of 'datasources'
-      datasourceUrl: process.env.DATABASE_URL,
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
