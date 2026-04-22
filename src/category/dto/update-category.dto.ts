1895010f6d8ad4e73fc0176e1b363d1b92d09a7a
Jaber Ali <jaberb281@gmail.com>
2026-04-22 15:25:48 +0500

    remove dead src/category folder

--- src/category/dto/update-category.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCategoryDto } from './create-category.dto'; // Ensure this name matches your file
-
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) { }
